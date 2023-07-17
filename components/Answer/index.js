dd0d0ed1c7e87fa3515832ee7748db9f5af6c811
Teresa <teresa.juengel@posteo.de>
2023-07-17 18:44:46 +0200

    readd error-message

--- components/Answer/index.js
+++ components/Answer/index.js
@@ -17,35 +17,35 @@ export default function Answer({
   additionalLife,
   personalAnswerText,
 }) {
-  // const acceptableValue = year >= 0 && today >= 0 && life >= 0;
+  const acceptableValue = year >= 0 && today >= 0 && life >= 0;
 
   return (
     <>
-      {/* {acceptableValue ? ( */}
-      <>
-        <PersonalAnswer
-          personalAnswerText={personalAnswerText}
-          year={year}
-          today={today}
-          life={life}
-          unit={unit}
-          {...(additionalYear !== undefined && { additionalYear })}
-          {...(additionalToday !== undefined && { additionalToday })}
-          {...(additionalLife !== undefined && { additionalLife })}
-        />
-        <PeriotopiaInfo periotopiaInfoText={periotopiaInfoText} />
-        {periotopiaIndexFinancials && (
-          <PeriotopiaIndex
-            periotopiaIndexFinancials={periotopiaIndexFinancials}
+      {acceptableValue ? (
+        <>
+          <PersonalAnswer
+            personalAnswerText={personalAnswerText}
+            year={year}
+            today={today}
+            life={life}
+            unit={unit}
+            {...(additionalYear !== undefined && { additionalYear })}
+            {...(additionalToday !== undefined && { additionalToday })}
+            {...(additionalLife !== undefined && { additionalLife })}
           />
-        )}
-      </>
-      {/* // ) : (
-      //   <p>
-      //     Es scheint etwas schief gelaufen zu sein. Bitte gehe zurück und fülle
-      //     die Fragen richtig und vollständig aus
-      //   </p>
-      // )} */}
+          <PeriotopiaInfo periotopiaInfoText={periotopiaInfoText} />
+          {periotopiaIndexFinancials && (
+            <PeriotopiaIndex
+              periotopiaIndexFinancials={periotopiaIndexFinancials}
+            />
+          )}
+        </>
+      ) : (
+        <p>
+          Es scheint etwas schief gelaufen zu sein. Bitte gehe zurück und fülle
+          die Fragen richtig und vollständig aus
+        </p>
+      )}
 
       <NavButton
         onClickQuestion={onClickQuestion}
