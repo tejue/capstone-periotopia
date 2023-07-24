3911088a9971b69d11893945ac9193c71302dd73
Teresa <teresa.juengel@posteo.de>
2023-07-24 20:15:42 +0200

    add styling to global

--- styles.js
+++ styles.js
@@ -1,6 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+  
+  :root {
+  
+  --backgroun-color: #fff7ff;
+  --primary-color: #aa1146;
+  --primary-color-text: #fff7ff;
+  --secondary-color-text: #343a40;
+  --highlight-color: #343a40;
+  --box-shadow: 0px -7px 20px rgba(238, 88, 141, 0.5);
+  --padding-sides: 30px;
+  --app-header-height: 60px;
+    }
+
   *,
   *::before,
   *::after {
@@ -10,6 +23,8 @@ export default createGlobalStyle`
   body {
     margin: 0;
     font-family: system-ui;
-    margin: 20px; 
+    background-color: var(--backgroun-color);
+    color: var(--secondary-color-text);
+    text-align: center; 
     }
 `;
