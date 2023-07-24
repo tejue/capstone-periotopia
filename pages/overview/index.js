def6a449ba47dadc3c131cfc03afd6c53c1c65c9
Teresa <teresa.juengel@posteo.de>
2023-07-24 20:14:39 +0200

    add landingpage, move overviewpage, add IntroComponent, style IntroComponent

---
+++ pages/overview/index.js
@@ -0,0 +1,45 @@
+import NewIndex from "@/components/NewIndex/index";
+import ResultCard from "@/components/ResultCard/index";
+
+export default function HomePage({
+  collectedOverviewResults,
+  handleDeleteResultCard,
+  formatNumber,
+  formatTime,
+}) {
+  return (
+    <>
+      <h1>Periotopia Overview</h1>
+      <NewIndex />
+      {collectedOverviewResults.map(
+        ({
+          id,
+          menstruationDaysPerYear,
+          costsPerYear,
+          taxAmount,
+          timePerYear,
+          access,
+          periotopiaIndexFinancials,
+          periotopiaIndexHygiene,
+        }) => {
+          return (
+            <ResultCard
+              key={id}
+              id={id}
+              formatNumber={formatNumber}
+              formatTime={formatTime}
+              costsPerYearID={costsPerYear}
+              periotopiaIndexHygieneID={periotopiaIndexHygiene}
+              periotopiaIndexFinancialsID={periotopiaIndexFinancials}
+              taxAmountID={taxAmount}
+              accessID={access}
+              handleDeleteResultCard={handleDeleteResultCard}
+              menstruationDaysPerYearID={menstruationDaysPerYear}
+              timePerYearID={timePerYear}
+            />
+          );
+        }
+      )}
+    </>
+  );
+}
