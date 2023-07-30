efe7623ca30fe74540a527c479d264f736c601dd
Teresa <teresa.juengel@posteo.de>
2023-07-30 15:03:27 +0200

    add small changes in wording

--- pages/intro/index.js
+++ pages/intro/index.js
@@ -45,7 +45,7 @@ export default function GeneralInfoPage({}) {
         />
       </Header>
       <FormField
-        question="Menstruierst du"
+        question="Menstruierst du?"
         type="radio"
         name="intro"
         options={optionsIntro}
@@ -54,10 +54,10 @@ export default function GeneralInfoPage({}) {
       />
 
       {currentValue.intro === "yes" && (
-        <PeriotopiaInfo periotopiaInfoText="Schön, dass du da bist! Hier kannst du herausfinden, welche Auswirkungen verschiedene Aspekte des Themas Menstruationsgesundheit auf deine eigene Lebensrealität haben, was anders sein könnte und wie es in Periotopia wäre. Falls du dir mal unsicher bei den Antworten bist, kannst du mit den einzelnen Buttons allgemeine Durchschnittswerte verwenden. Viel Spaß." />
+        <PeriotopiaInfo periotopiaInfoText="Schön, dass du da bist! Hier kannst du herausfinden, wie verschiedene Bereich des Themas Menstruationsgesundheit auf deine Lebensrealität wirken, was anders sein könnte und wie es in Periotopia wäre. Falls du dir mal unsicher bei den Antworten bist, kannst du mit dem jeweiligen Button immer auch allgemeine Durchschnittswerte verwenden. Viel Spaß." />
       )}
       {currentValue.intro === "no" && (
-        <PeriotopiaInfo periotopiaInfoText="Schön, dass du da bist! Falls du es dich fragts: ja, auch du kannst schauen, was in Periotopia anders wäre. Falls du dir mal unsicher bist bei den Antworten bist, kannst du natürlich immer eine dir nahstehende, menstruierende Person fragen. Du hast aber auch die Möglichkeit, mit den einzelnen Buttons allgemeine Durchschnittswerte verwenden. Viel Spaß." />
+        <PeriotopiaInfo periotopiaInfoText="Schön, dass du da bist! Ja, auch du kannst schauen, was in Periotopia anders wäre. Falls du dir mal unsicher bei den Antworten bist, kannst du natürlich immer eine dir nahstehende, menstruierende Person fragen. Du hast aber auch die Möglichkeit, mit dem jeweiligen Buttons allgemeine Durchschnittswerte zu verwenden. Viel Spaß." />
       )}
       <></>
       {currentValue.intro !== "" && (
